ab6a2650f754483f400a7ab5c5a09852924b94be
Pavel <pavel@ya.ru>
2021-02-19 22:54:09 +0300

    нарисовал график из потока байт! через html!

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from views import index
+from views import index, stream_show
 
 def setup_routes(app):
     app.router.add_get('/', index)
+    app.router.add_get('/ws', stream_show)
 
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 import aiohttp_jinja2
 import sqlite3
+import base64
 
 from system_logger import make_image_by_dots
 
@@ -13,28 +14,22 @@ def get_value_from_database():
     # print(cpu)
     return cpu
 
-@aiohttp_jinja2.template('index.html')
+#
+# @aiohttp_jinja2.template('index.html')
+# async def index(request):
+#     data = get_value_from_database()
+#     img_bytes = make_image_by_dots(data)
+#     content = img_bytes.getvalue()
+#     print(f'cpu:len {len(data)}')
+#     return {'cpu_stats': data, 'img': content}
+#     # return web.Response(body=content, content_type='image/png')
+
 async def index(request):
     data = get_value_from_database()
     img_bytes = make_image_by_dots(data)
-    content = img_bytes.getvalue()
-    print(f'cpu:len {len(data)}')
-    # return {'cpu_stats': data, 'img': path_to_img}
-    return web.Response(body=content, content_type='image/png')
+    # img_tag = "<img src='data:image/png;base64,'" + img_buffer.getvalue() + "</img>"
+    img_tag = "<img src='data:image/png;base64," + base64.b64encode(img_bytes.getvalue()).decode() + "'/>"
+    return web.Response(body=img_tag, content_type='text/html')
 
-
-#
-# @router.get("/{post}/image")
-# async def render_post_image(request: web.Request) -> web.Response:
-#     post_id = request.match_info["post"]
-#     db = request.config_dict["DB"]
-#     async with db.execute("SELECT image FROM posts WHERE id = ?", [post_id]) as cursor:
-#         row = await cursor.fetchone()
-#         if row is None or row["image"] is None:
-#             img = PIL.Image.new("RGB", (64, 64), color=0)
-#             fp = io.BytesIO()
-#             img.save(fp, format="JPEG")
-#             content = fp.getvalue()
-#         else:
-#             content = row["image"]
-#     return web.Response(body=content, content_type="image/jpeg")
+async def stream_show(request):
+    return web.Response(text='Hello')
