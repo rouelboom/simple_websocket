df46da49c4b6acec59bd2f6fb2267ef67b6c5455
Pavel <pavel@ya.ru>
2021-02-22 02:44:26 +0300

    pep8 fix

--- system_logger.py
+++ system_logger.py
@@ -11,14 +11,14 @@ AXIS_X_LENGTH = 50
 Y_SCALE = 5
 X_SCALE = 1.5
 
-IMG_WIDTH = 1130
+IMG_WIDTH = 1180
 IMG_HIGTH = 550
+
 
 def init_database():
     db_connection = sqlite3.connect('system-loading.db.sqlite',
-                                     detect_types=sqlite3.PARSE_DECLTYPES |
-                                                  sqlite3.PARSE_COLNAMES
-                                    )
+                                    detect_types=sqlite3.PARSE_DECLTYPES |
+                                    sqlite3.PARSE_COLNAMES)
 
     cursor = db_connection.cursor()
     cursor.execute('''
@@ -74,32 +74,26 @@ def make_image_by_dots(points: list):
     prepare_for_paint(points)
 
     for i in range(len(points) - 1):
-        x1 = transform_X_coord_to_asixs(i)
-        x2 = transform_X_coord_to_asixs(i + 1)
-        y1 = transform_Y_coord_to_asixs(points[i][0])
-        y2 = transform_Y_coord_to_asixs(points[i + 1][0] )
+        x1 = transform_x_coord_to_asixs(i)
+        x2 = transform_x_coord_to_asixs(i + 1)
+        y1 = transform_y_coord_to_asixs(points[i][0])
+        y2 = transform_y_coord_to_asixs(points[i + 1][0])
 
         draw.line((x1, y1, x2, y2), fill=ImageColor.getrgb('red'))
 
     for i in range(len(points) - 1):
-        x1 = transform_X_coord_to_asixs(i)
-        x2 = transform_X_coord_to_asixs(i + 1)
-        y1 = transform_Y_coord_to_asixs(points[i][1])
-        y2 = transform_Y_coord_to_asixs(points[i + 1][1])
+        x1 = transform_x_coord_to_asixs(i)
+        x2 = transform_x_coord_to_asixs(i + 1)
+        y1 = transform_y_coord_to_asixs(points[i][1])
+        y2 = transform_y_coord_to_asixs(points[i + 1][1])
 
         draw.line((x1, y1, x2, y2), fill=ImageColor.getrgb('green'))
 
-    img_file= BytesIO()
+    img_file = BytesIO()
     image.save(img_file, format="PNG")
     img_file.seek(0)
 
     return img_file
-
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
 
 def make_draw_area():
@@ -107,10 +101,12 @@ def make_draw_area():
         и прочую вспомогательную информацию"""
     image = Image.new("RGB", (IMG_WIDTH, IMG_HIGTH))
     draw = ImageDraw.Draw(image)
-    draw.rectangle((0, 0, IMG_WIDTH, IMG_HIGTH), fill=ImageColor.getrgb("white"))
+    draw.rectangle((0, 0, IMG_WIDTH, IMG_HIGTH),
+                   fill=ImageColor.getrgb("white"))
 
-    draw.line((AXIS_X_LENGTH - 15, AXIS_Y_LENGTH, IMG_WIDTH - 50, AXIS_Y_LENGTH),
-              fill=ImageColor.getrgb("black")) # ось Х
+    draw.line((AXIS_X_LENGTH - 15, AXIS_Y_LENGTH,
+              IMG_WIDTH - 50, AXIS_Y_LENGTH),
+              fill=ImageColor.getrgb("black"))  # ось Х
     draw.line((AXIS_X_LENGTH, 0, AXIS_X_LENGTH, AXIS_Y_LENGTH + 15),
               fill=ImageColor.getrgb("black"))  # ось Y
     myfont = ImageFont.truetype("arial.ttf", 20)
@@ -120,11 +116,11 @@ def make_draw_area():
     return image, draw
 
 
-def transform_Y_coord_to_asixs(y: float):
+def transform_y_coord_to_asixs(y: float):
     return AXIS_Y_LENGTH - (y * Y_SCALE)
 
 
-def transform_X_coord_to_asixs(x: float):
+def transform_x_coord_to_asixs(x: float):
     return AXIS_X_LENGTH + (x * X_SCALE)
 
 
@@ -132,7 +128,7 @@ def prepare_for_paint(data):
     times = []
 
     for i in range(len(data)):
-        times.append(data[i][2])
+        times.append(dt.datetime.strptime(data[i][2], '%Y-%m-%d %H:%M:%S.%f'))
 
-    # for i in range(len(times) - 2):
-        # print(times[i + 2] - times[i])
+    # for i in range(len(times) - 1):
+        # print(times[i + 1] - times[i])
