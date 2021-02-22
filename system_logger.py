0f81ba8ee9dee85cb59fddd40821603815c8afa3
Pavel <pavel@ya.ru>
2021-02-22 04:10:12 +0300

    подправил html

--- system_logger.py
+++ system_logger.py
@@ -59,7 +59,7 @@ def get_value_from_database():
     cursor.execute(s, [last_hour])
 
     data = cursor.fetchall()
-    print(len(data))
+    # print(len(data))
     return data
 
 
@@ -82,7 +82,7 @@ def make_image_by_dots(points: list):
 
         draw.line((x1, y1, x2, y2), fill=ImageColor.getrgb('red'))
 
-    for i in range(len(points) - 1):
+    for i in range(len(mem) - 1):
         x1 = transform_x_coord_to_asixs(i)
         x2 = transform_x_coord_to_asixs(i + 1)
         y1 = transform_y_coord_to_asixs(mem[i])
@@ -136,28 +136,31 @@ def prepare_for_paint(data):
     y_cpu = []
     a = 0
     b = 0
-    for i in range(len(data) - 1):
-        # Если наблюдается время между данными больше 6(доп.секунда
-        # на асинхронные операции) сек - в буффер
-        # добавляется нулевое значение логированного параметра
-        if times[i + 1] - times[i] > dt.timedelta(seconds=6):
-            b += 1
-            print('b=', b)
-            print((times[i + 1] - times[i]))
-            delta = ((times[i + 1] - times[i]).seconds / 5)
-            print('delta' + str(delta))
-            for j in range(int(delta)):
-                a += 1
-                print('a=', a)
-                y_mem.append(0)
-                y_cpu.append(0)
-        y_mem.append(data[i][1])
-        y_cpu.append(data[i][0])
+    if(len(data) < 720):
+        for i in range(len(data) - 1):
+            # Если наблюдается время между данными больше 6(доп.секунда
+            # на асинхронные операции) сек - в буффер
+            # добавляется нулевое значение логированного параметра
+            if times[i + 1] - times[i] > dt.timedelta(seconds=6):
+                b += 1
+                # print('b=', b)
+                # print((times[i + 1] - times[i]))
+                delta = ((times[i + 1] - times[i]).seconds / 5)
+                # print('delta' + str(delta))
+                for j in range(int(delta)):
+                    a += 1
+                    # print('a=', a)
+                    y_mem.append(0)
+                    y_cpu.append(0)
+            y_mem.append(data[i][1])
+            y_cpu.append(data[i][0])
 
-    last = len(data) - 1
-    y_mem.append(data[last][1])
-    y_cpu.append(data[last][0])
+        last = len(data) - 1
+        y_mem.append(data[last][1])
+        y_cpu.append(data[last][0])
 
-    print(len(y_mem))
-    print(len(y_cpu))
-    return y_cpu, y_mem
+        # print(len(y_mem))
+        # print(len(y_cpu))
+        return y_cpu, y_mem
+
+    return data[0], data[1]
