5f0c29ae396e0e3afe5a0bad8dfd397ebb05867d
Pavel <pavel@ya.ru>
2021-02-22 03:50:47 +0300

    надо разобраться с лишними нулевыми данными

--- system_logger.py
+++ system_logger.py
@@ -71,21 +71,22 @@ def make_image_by_dots(points: list):
         рисуется изображение.
         Функция возвращает изображение в байтовом виде"""
     image, draw = make_draw_area()
-    prepare_for_paint(points)
+    cpu, mem = prepare_for_paint(points)
 
-    for i in range(len(points) - 1):
+    for i in range(len(cpu) - 1):
+
         x1 = transform_x_coord_to_asixs(i)
         x2 = transform_x_coord_to_asixs(i + 1)
-        y1 = transform_y_coord_to_asixs(points[i][0])
-        y2 = transform_y_coord_to_asixs(points[i + 1][0])
+        y1 = transform_y_coord_to_asixs(cpu[i])
+        y2 = transform_y_coord_to_asixs(cpu[i + 1])
 
         draw.line((x1, y1, x2, y2), fill=ImageColor.getrgb('red'))
 
     for i in range(len(points) - 1):
         x1 = transform_x_coord_to_asixs(i)
         x2 = transform_x_coord_to_asixs(i + 1)
-        y1 = transform_y_coord_to_asixs(points[i][1])
-        y2 = transform_y_coord_to_asixs(points[i + 1][1])
+        y1 = transform_y_coord_to_asixs(mem[i])
+        y2 = transform_y_coord_to_asixs(mem[i + 1])
 
         draw.line((x1, y1, x2, y2), fill=ImageColor.getrgb('green'))
 
@@ -125,10 +126,38 @@ def transform_x_coord_to_asixs(x: float):
 
 
 def prepare_for_paint(data):
+    """ Функция решает вопрос с недостающими данными за последний час """
     times = []
 
     for i in range(len(data)):
         times.append(dt.datetime.strptime(data[i][2], '%Y-%m-%d %H:%M:%S.%f'))
 
-    # for i in range(len(times) - 1):
-        # print(times[i + 1] - times[i])
+    y_mem = []
+    y_cpu = []
+    a = 0
+    b = 0
+    for i in range(len(data) - 1):
+        # Если наблюдается время между данными больше 6(доп.секунда
+        # на асинхронные операции) сек - в буффер
+        # добавляется нулевое значение логированного параметра
+        if times[i + 1] - times[i] > dt.timedelta(seconds=6):
+            b += 1
+            print('b=', b)
+            print((times[i + 1] - times[i]))
+            delta = ((times[i + 1] - times[i]).seconds / 5)
+            print('delta' + str(delta))
+            for j in range(int(delta)):
+                a += 1
+                print('a=', a)
+                y_mem.append(0)
+                y_cpu.append(0)
+        y_mem.append(data[i][1])
+        y_cpu.append(data[i][0])
+
+    last = len(data) - 1
+    y_mem.append(data[last][1])
+    y_cpu.append(data[last][0])
+
+    print(len(y_mem))
+    print(len(y_cpu))
+    return y_cpu, y_mem
